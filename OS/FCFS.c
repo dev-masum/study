d4957506a5a8196072ca8e7ed240e2893437fd78
Abdullah al Masum <abdullahalmasum.dev@gmail.com>
2024-01-27 19:42:57 +0600

    some issues fixed

--- OS/FCFS.c
+++ OS/FCFS.c
@@ -1,42 +1,41 @@
-#include<stdio.h>
+#include <stdio.h>
 
- int main()
-
+int main()
 {
-    int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
+    int n, bt[20], wt[20], tat[20], avwt = 0, avtat = 0, i, j;
     printf("Enter total number of processes(maximum 20):");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    printf("n\Enter Process Burst Time\n");
-    for(i=0;i<n;i++)
+    printf("\nEnter Process Burst Time\n");
+    for (i = 0; i < n; i++)
     {
-        printf("P[%d]:",i+1);
-        scanf("%d",&bt[i]);
+        printf("P[%d]:", i + 1);
+        scanf("%d", &bt[i]);
     }
 
-    wt[0]=0;
+    wt[0] = 0;
 
-    for(i=1;i<n;i++)
+    for (i = 1; i < n; i++)
     {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
+        wt[i] = 0;
+        for (j = 0; j < i; j++)
+            wt[i] += bt[j];
     }
 
-    printf("n\Process\t Burst Time\t Waiting Time\t Turnaround Time");
+    printf("\nProcess\t Burst Time\t Waiting Time\t Turnaround Time");
 
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-        tat[i]=bt[i]+wt[i];
-        avwt+=wt[i];
-        avtat+=tat[i];
-        printf("\nP[%d]\t\t %d\t\t %d\t\t %d",i+1,bt[i],wt[i],tat[i]);
+        tat[i] = bt[i] + wt[i];
+        avwt += wt[i];
+        avtat += tat[i];
+        printf("\nP[%d]\t\t %d\t\t %d\t\t %d", i + 1, bt[i], wt[i], tat[i]);
     }
 
-    avwt/=i;
-    avtat/=i;
-    printf("\n\nAverage Waiting Time:%d",avwt);
-    printf("\nAverage Turnaround Time:%d",avtat);
+    avwt /= i;
+    avtat /= i;
+    printf("\n\nAverage Waiting Time:%d", avwt);
+    printf("\nAverage Turnaround Time:%d", avtat);
 
     return 0;
 }
